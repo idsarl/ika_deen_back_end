a5353ccf052ed62b636a18f6f935a9ea2259e2e3
Deku5005 <dolooumar60@gmail.com>
2026-05-07 21:58:53 +0000

    Ajustement

--- src/main/java/ika_deen/back_end/BackEndApplication.java
+++ src/main/java/ika_deen/back_end/BackEndApplication.java
@@ -1,5 +1,6 @@
 package ika_deen.back_end;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackEndApplication {
 
 	public static void main(String[] args) {
+		// Charger les variables d'environnement depuis le fichier .env s'il existe
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		
 		SpringApplication.run(BackEndApplication.class, args);
 	}
 
--- src/main/java/ika_deen/back_end/controller/AuthenticationController.java
+++ src/main/java/ika_deen/back_end/controller/AuthenticationController.java
@@ -7,10 +7,7 @@ import ika_deen.back_end.service.AuthenticationService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Controller gérant les endpoints d'authentification.
@@ -19,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthenticationController {
 
     private final AuthenticationService service;
